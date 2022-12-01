a1d512e5462e7be982d14c138782f4053f8a552d
Thifany Garcia <100360093+projetosthif@users.noreply.github.com>
2022-12-01 10:55:27 -0300

    Add files via upload

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,23 +1,23 @@
 function insere(numero){
-    var valor = document.getElementById ('resultado').ineerHTML;
-    document.getElementById('resultado').innerHTML = valor + numero; 
+    var valor = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = valor + numero;
 }
 
 function limpar(){
-    document.getElementById ('resultado').innerHTML = "";
+    document.getElementById('resultado').innerHTML = "";
 }
 
 function voltar(){
     var resultado = document.getElementById('resultado').innerHTML;
-    document.getElementById('resultado').innerHTML = resultado.substring(0,resultado.length - 1);
+    document.getElementById('resultado').innerHTML = resultado.substring(0,resultado.length -1);
 }
 
 function calcular(){
     var resultado = document.getElementById('resultado').innerHTML;
-    if (resultado){
+    if(resultado){
         document.getElementById('resultado').innerHTML = eval(resultado);
     }
     else{
-        document.getElementById('resultado').innerHTML = "Nada...";
+        document.getElementById('resultado').innerHTML = "Nada ...";
     }
 }
